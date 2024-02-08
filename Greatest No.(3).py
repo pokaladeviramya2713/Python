01eaaa15390619ac7f6ea0245eaa6f8899c09537
pokaladeviramya2713 <130475902+pokaladeviramya2713@users.noreply.github.com>
2024-02-08 10:01:48 +0530

    Add files via upload

---
+++ Greatest No.(3).py
@@ -0,0 +1,10 @@
+num1=5
+num2=6
+num3=7
+if(num1>=num2)and (num1>=num3):
+    largest=num1
+elif(num2>=num1)and (num2>=num3):
+        largest=num2
+else:
+            largest=num3
+            print("The largest is",largest)
