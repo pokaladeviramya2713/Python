6e74255d35a16be31bfd3fe7f0214e918dab475d
pokaladeviramya2713 <130475902+pokaladeviramya2713@users.noreply.github.com>
2024-02-08 10:05:48 +0530

    Add files via upload

---
+++ Marks.py
@@ -0,0 +1,11 @@
+percentage=int(input("Enter a number"))
+if (percentage>=90):
+    print("Out Standing")
+elif(percentage>=80):
+    print("Exellent")
+elif(percentage>=65):
+    print("Good")
+elif(percentage>=50):
+    print("Pass")
+else:
+    print("Fail")
